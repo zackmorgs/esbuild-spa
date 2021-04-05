7aeb5ecc25e7d0f5b4d2bc56da425777920004a5
Zack Morgenthaler <zackm@csionsite.com>
2021-04-05 13:04:02 -0500

    deleted some stupid stuff, created todo for mysellf in index.js

--- index.js
+++ index.js
@@ -11,13 +11,30 @@ let spa = fs.readFileSync(path.resolve("./build/index.html"), 'utf8', (err) => {
     if (err) { throw err; }
 });
 
+let script = fs.readFileSync(path.resolve("./build/js/script.js"), 'utf8', (err) => {
+    if (err) { throw err; }
+});
+
+// todo:
+// create a hashtable like object that gets the files from the directory.
+// create hashtable via adding as children to array (foreach file in dir)
+
 console.log(spa);
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     //   res.end(req.url);
+    let requestUrl = req.url;
+
+    // if (requestUrl.indexOf('.js') || requestUrl.i) {
+    //     res.end()
+    // }
+
+    // todo: currently returns the spa html, but non of your .css/.js is being loaded because request keeps giving back html instead. 
+
     res.end(spa);
+
 });
 
 // server.listen(port, hostname, () => {
