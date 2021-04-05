1f59548388a18452e541389bff554ec0d880400e
Zack Morgenthaler <zackm@csionsite.com>
2021-04-05 12:58:27 -0500

    wrestling with node http... backing up to remote

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const http = require('http');
-const http2 = require('http2');
+// const http2 = require('http2');
+// todo
 const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let spa = fs.readFileSync(path.resolve("./build/index.html"), 'utf8', (err) => {
+    if (err) { throw err; }
+});
+
+console.log(spa);
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    //   res.end(req.url);
+    res.end(spa);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// const server = http2.createSecureServer({
-//     key: fs.readFileSync('localhost-privkey.pem'),
-//     cert: fs.readFileSync('localhost-cert.pem')
-//   });
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 import ReactDOM from 'react-dom';
 import * as Server from 'react-dom/server'
 
-let Greet = () => <h1>Hello, world!</h1>
+let Greet = () => <h1>Hiya! Path: {document.location.pathname}</h1>
 
-console.log(Server.renderToString(<Greet />))
+// console.log(Server.renderToString(<Greet />))
 
 ReactDOM.render(<Greet/>, document.getElementById('root'));
